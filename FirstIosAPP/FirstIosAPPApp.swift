4f38739beb09901a8eebee2d4e0c0331228e2153
Rohit Singh <73903627+rohitttsingh@users.noreply.github.com>
2022-04-26 16:57:33 +0530

    Initial Commit

---
+++ FirstIosAPP/FirstIosAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstIosAPPApp.swift
+//  FirstIosAPP
+//
+//  Created by Rohit Singh on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstIosAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
